b8a3b24d2fcb828464927bbe4cb58cafad3673c6
alvinpradana <alvinpradana14@gmail.com>
2023-08-20 22:52:47 +0700

    update route link

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/login', function () {
     return view('login');
