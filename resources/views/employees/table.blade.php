8429497caf0152aa758f2b1ba3db5cb98ef30004
alvinpradana <alvinpradana14@gmail.com>
2023-08-21 22:53:55 +0700

    create employee controller and view

---
+++ resources/views/employees/table.blade.php
@@ -0,0 +1,40 @@
+<p class="mb-4">Tabel berikut merupakan data karyawan pada SMK Crows Zero tahun ajaran 2023/2024.</p>
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Data Karyawan</h6>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Posisi</th>
+                        <th>Gender</th>
+                        <th>Usia</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Mas John</td>
+                        <td>Office Boy</td>
+                        <td>L</td>
+                        <td>30</td>
+                        <td>Active</td>
+                        <td>$145,600</td>
+                    </tr>
+                    <tr>
+                        <td>Brodin</td>
+                        <td>Security</td>
+                        <td>L</td>
+                        <td>35</td>
+                        <td>Active</td>
+                        <td>$145,600</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
