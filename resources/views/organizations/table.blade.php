a5bf52d2ae1a52099f59c12d50c4c75f6bda405c
alvinpradana <alvinpradana14@gmail.com>
2023-08-21 22:54:31 +0700

    create organization controller and view

---
+++ resources/views/organizations/table.blade.php
@@ -0,0 +1,37 @@
+<p class="mb-4">Tabel berikut merupakan data organisasi pelajar pada SMK Crows Zero tahun ajaran 2023/2024.</p>
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Data Organisasi</h6>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Bidang</th>
+                        <th>Jumlah Anggota</th>
+                        <th>Ketua</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>KADOL</td>
+                        <td>Percuanan</td>
+                        <td>20 Orang</td>
+                        <td>Yanto</td>
+                        <td>$145,600</td>
+                    </tr>
+                    <tr>
+                        <td>AMCC</td>
+                        <td>Komputer</td>
+                        <td>15 Orang</td>
+                        <td>Kalis</td>
+                        <td>$145,600</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
