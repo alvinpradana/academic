0914bb2e0589f74e7833db2c23fc7b23c08f7c53
alvinpradana <alvinpradana14@gmail.com>
2023-08-21 22:53:35 +0700

    create class controller and view

---
+++ resources/views/class/table.blade.php
@@ -0,0 +1,34 @@
+<p class="mb-4">Tabel berikut merupakan data kelas pada SMK Crows Zero tahun ajaran 2023/2024.</p>
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Data Kelas</h6>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Kelas</th>
+                        <th>Ruang</th>
+                        <th>Jumlah Siswa</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>7</td>
+                        <td>A</td>
+                        <td>20 Orang</td>
+                        <td>$145,600</td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>A</td>
+                        <td>20 Orang</td>
+                        <td>$145,600</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
