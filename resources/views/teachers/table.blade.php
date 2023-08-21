cb105088b91173d2b0e000b8e2abef0f38b389e9
alvinpradana <alvinpradana14@gmail.com>
2023-08-21 22:55:05 +0700

    create teacher controller and view

---
+++ resources/views/teachers/table.blade.php
@@ -0,0 +1,40 @@
+<p class="mb-4">Tabel berikut merupakan data pengajar pada SMK Crows Zero tahun ajaran 2023/2024.</p>
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Data Pengajar</h6>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Position</th>
+                        <th>Kelas</th>
+                        <th>Age</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Prof. Yanto</td>
+                        <td>Rektor Bruhh</td>
+                        <td>9A</td>
+                        <td>46</td>
+                        <td>Active</td>
+                        <td>$145,600</td>
+                    </tr>
+                    <tr>
+                        <td>Mas Sudarmawan</td>
+                        <td>Wakil Rektor</td>
+                        <td>9B</td>
+                        <td>50</td>
+                        <td>Active</td>
+                        <td>$145,600</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
