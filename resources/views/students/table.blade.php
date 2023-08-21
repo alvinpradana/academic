ec71cf03a9248d54ef8701b0282abb1f6f5d828c
alvinpradana <alvinpradana14@gmail.com>
2023-08-21 22:54:47 +0700

    create student controller and view

---
+++ resources/views/students/table.blade.php
@@ -0,0 +1,40 @@
+<p class="mb-4">Tabel berikut merupakan data pelajar pada SMK Crows Zero tahun ajaran 2023/2024.</p>
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Data Pelajar</h6>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Kejuruan</th>
+                        <th>Kelas</th>
+                        <th>Gender</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>John Doe</td>
+                        <td>Fisika</td>
+                        <td>9A</td>
+                        <td>L</td>
+                        <td>Active</td>
+                        <td>$145,600</td>
+                    </tr>
+                    <tr>
+                        <td>Mas Al</td>
+                        <td>Komputer</td>
+                        <td>9A</td>
+                        <td>L</td>
+                        <td>Active</td>
+                        <td>$145,600</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
