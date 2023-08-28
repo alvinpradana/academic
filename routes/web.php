66bbc0185f3ae0533d6dff0614a7db633def0b63
alvinpradana <alvinpradana14@gmail.com>
2023-08-28 22:20:03 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,17 @@ use App\Http\Controllers\LessonsController;
 use App\Http\Controllers\EmployeesController;
 use App\Http\Controllers\OrganizationsController;
 use App\Http\Controllers\ClassController;
+use App\Http\Controllers\GradesController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/teachers', [TeachersController::class, 'index']);
-Route::get('/students', [StudentsController::class, 'index']);
-Route::get('/lessons', [LessonsController::class, 'index']);
-Route::get('/employees', [EmployeesController::class, 'index']);
-Route::get('/organizations', [OrganizationsController::class, 'index']);
-Route::get('/class', [ClassController::class, 'index']);
+Route::resource('dashboard', HomeController::class);
+Route::resource('teachers', TeachersController::class);
+Route::resource('students', StudentsController::class);
+Route::resource('employees', EmployeesController::class);
+Route::resource('lessons', LessonsController::class);
+Route::resource('organizations', OrganizationsController::class);
+Route::resource('class', ClassController::class);
+Route::resource('grades', GradesController::class);
 
 Route::get('/login', function () {
     return view('login');
