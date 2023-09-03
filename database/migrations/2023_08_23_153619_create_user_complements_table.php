3f5c51f8b15e77a6c19bef21d93bacf4d8405d5b
alvinpradana <alvinpradana14@gmail.com>
2023-09-03 13:05:59 +0700

    update user complement migration

--- database/migrations/2023_08_23_153619_create_user_complements_table.php
+++ database/migrations/2023_08_23_153619_create_user_complements_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('name');
+            $table->string('id_number');
+            $table->string('nip_number');
             $table->text('image')->nullable();
             $table->enum('gender', ['male', 'female']);
             $table->double('age');
