e3ee80cf04b3b5a07865b3a682aadec0deb47d8e
alvinpradana <alvinpradana14@gmail.com>
2023-09-03 13:04:48 +0700

    update routing web

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\OrganizationsController;
 use App\Http\Controllers\ClassController;
 use App\Http\Controllers\ClassLevelController;
 use App\Http\Controllers\GradesController;
+use App\Http\Controllers\MajorController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('dashboard', HomeController::class);
@@ -20,6 +21,7 @@ Route::resource('organizations', OrganizationsController::class);
 Route::resource('class', ClassController::class);
 Route::resource('class-level', ClassLevelController::class);
 Route::resource('grades', GradesController::class);
+Route::resource('majors', MajorController::class);
 
 Route::get('/login', function () {
     return view('login');
