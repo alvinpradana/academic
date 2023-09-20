95d9870d4c1a930c0498638414adb0ad16d50a34
alvinpradana <alvinpradana14@gmail.com>
2023-09-20 21:38:27 +0700

    renamed scholarship table

--- database/migrations/2023_09_18_155442_create_scholarships_table.php
+++ database/migrations/2023_09_18_155442_create_scholarships_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('scolarships', function (Blueprint $table) {
+        Schema::create('scholarships', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('notes');
