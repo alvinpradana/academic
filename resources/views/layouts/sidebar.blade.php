19576283651281b30e8de96f3447c0fd0715121f
alvinpradana <alvinpradana14@gmail.com>
2023-09-20 19:44:58 +0700

    update sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -14,7 +14,7 @@
     </li>
     <hr class="sidebar-divider">
     <div class="sidebar-heading">
-        Pelajar
+        Kesiswaan
     </div>
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#studentsTab" aria-expanded="true" aria-controls="studentsTab">
@@ -38,6 +38,7 @@
         <div id="classTab" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Data Kelas</h6>
+                <a class="collapse-item" href="{{ route('class.index') }}">Semua Kelas</a>
                 <a class="collapse-item" href="{{ route('class.index') }}">Data Kelas</a>
                 <a class="collapse-item" href="{{ route('class-level.index') }}">Level Kelas</a>
                 <a class="collapse-item" href="{{ route('majors.index') }}">Kejuruan</a>
