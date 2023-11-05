a2ad8c89e1df9e48ae7e436c77ba5b02841fa9fd
alvinpradana <alvinpradana14@gmail.com>
2023-11-05 21:47:35 +0700

    add home class group data

---
+++ app/Http/Controllers/ClassGroupController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Classes;
+use App\Models\ClassGroup;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ClassGroupController extends Controller
+{
+    public function index() {
+        $class = Classes::with([
+            'majors',
+            'class_levels',
+            'grades'
+        ])->orderBy('id', 'asc')->get();
+
+        return view('class.group.home', ['class' => $class]);
+    }
+
+    public function show($id) {
+        $class_group = ClassGroup::where('class_id', $id)->with([
+            'class.class_levels',
+            'class.grades',
+            'class.majors'
+        ])->get();
+
+        $students = ClassGroup::where('class_id', $id)->with('users.user_complements')->get();
+        $count = $students->sum('id');
+
+        // $class_level = $class_group->class->class_levels->level;
+        // $class_grade = $class_group->class->grades->title;
+        // $class_major = $class_group->class->majors->title;
+
+        // $current_class = $class_level . ' ' . $class_major . ' ' . $class_grade;
+        dd($class_group);
+
+        return view('class.group.show', [
+            'count' => $count,
+            'students' => $students,
+            // 'class' => $current_class
+        ]);
+    }
+
+    public function create() {
+        return view('class.group.create');
+    }
+}
---
+++ resources/views/class/group/home.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app', ['title' => 'Data Kelas Siswa'])
+
+@section('content')
+    <div>
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Data Semua Kelas</h1>
+            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+                <i class="fas fa-download fa-sm text-white-50"></i> Generate Report
+            </a>
+        </div>
+        <div class="row">
+            @foreach ($class as $item)
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <a href="{{ route('class-group.show', $item->id) }}" style="text-decoration: none; cursor: pointer;" class="cursor-pointer">
+                        <div class="card border-left-primary shadow h-100 py-2">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                            Kelas
+                                        </div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                            {{ $item->class_levels->level }} {{ $item->majors->title }} {{ $item->grades->title }}
+                                        </div>
+                                    </div>
+                                    <div class="col-auto">
+                                        <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
