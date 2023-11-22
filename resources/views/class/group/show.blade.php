93392a61da0cbb62dbbe04f490f48fbdc96f3177
alvinpradana <alvinpradana14@gmail.com>
2023-11-22 21:55:49 +0700

    update alert session show class group page

--- resources/views/class/group/show.blade.php
+++ resources/views/class/group/show.blade.php
@@ -26,9 +26,9 @@
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>
-    @elseif (session('warning'))
+    @elseif (session('error'))
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            {{ session('warning') }}
+            {{ session('error') }}
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
