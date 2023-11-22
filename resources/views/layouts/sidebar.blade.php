0f7e6b7166b4d51ae7212851a109d99e25365816
alvinpradana <alvinpradana14@gmail.com>
2023-11-22 22:22:54 +0700

    update sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -14,6 +14,27 @@
     </li>
     <hr class="sidebar-divider">
     <div class="sidebar-heading">
+        Akademik
+    </div>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#academicTab" aria-expanded="true" aria-controls="academicTab">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>Data Akademik</span>
+        </a>
+        <div id="academicTab" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Data Akademik</h6>
+                <a class="collapse-item" href="{{ route('students.index') }}">Data Semester</a>
+                <a class="collapse-item" href="{{ route('majors.index') }}">Data Jurusan</a>
+                <a class="collapse-item" href="{{ route('class-level.index') }}">Data Level Kelas</a>
+                <a class="collapse-item" href="{{ route('grades.index') }}">Data Grade Kelas</a>
+                <a class="collapse-item" href="{{ route('students.index') }}">Data Nilai</a>
+                <a class="collapse-item" href="{{ route('students.index') }}">Data Presensi</a>
+            </div>
+        </div>
+    </li>
+    <hr class="sidebar-divider">
+    <div class="sidebar-heading">
         Kesiswaan
     </div>
     <li class="nav-item">
@@ -25,8 +46,6 @@
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Data Pelajar</h6>
                 <a class="collapse-item" href="{{ route('students.index') }}">Data Siswa</a>
-                <a class="collapse-item" href="{{ route('students.index') }}">Data Nilai</a>
-                <a class="collapse-item" href="{{ route('students.index') }}">Data Presensi</a>
             </div>
         </div>
     </li>
@@ -40,9 +59,6 @@
                 <h6 class="collapse-header">Data Kelas</h6>
                 <a class="collapse-item" href="{{ route('class-group.index') }}">Semua Kelas</a>
                 <a class="collapse-item" href="{{ route('class.index') }}">Data Kelas</a>
-                <a class="collapse-item" href="{{ route('majors.index') }}">Kejuruan</a>
-                <a class="collapse-item" href="{{ route('class-level.index') }}">Level Kelas</a>
-                <a class="collapse-item" href="{{ route('grades.index') }}">Grade Kelas</a>
             </div>
         </div>
     </li>
