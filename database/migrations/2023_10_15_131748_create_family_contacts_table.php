67f12478a0c830742b4dd4d8522f193afcccccb6
alvinpradana <alvinpradana14@gmail.com>
2024-01-03 21:14:54 +0700

    update table family contact

--- database/migrations/2023_10_15_131748_create_family_contacts_table.php
+++ database/migrations/2023_10_15_131748_create_family_contacts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('name')->nullable();
-            $table->enum('status', ['A', 'B', 'C', 'D'])->default('D');
+            $table->enum('status', ['A', 'B', 'C', 'D'])->default('D')->nullable();
             $table->unsignedBigInteger('phone_number')->nullable();
             $table->string('address')->nullable();
             $table->string('subdistrict')->nullable();
