fb0926e83f7a607242ee76027c10dadc3aeb8c38
alvinpradana <alvinpradana14@gmail.com>
2024-03-18 21:51:04 +0700

    add modal confirm to delete position

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,15 +1,19 @@
 <div class="modal fade" id="modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Data</h1>
+                <h5 class="modal-title" id="exampleModalLabel">Delete Confirmation!</h5>
             </div>
             <div class="modal-body">
                 {{ $body ?? '' }}
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <form action="{{ $route ?? '#' }}" method="post" class="d-inline">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-primary">Delete</button>
+                </form>
             </div>
         </div>
     </div>
