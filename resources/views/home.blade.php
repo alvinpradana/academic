8c8ed373d095d212b0ca391cfd659046316fa400
alvinpradana <alvinpradana14@gmail.com>
2024-03-20 21:40:09 +0700

    add url link item dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,72 +10,80 @@
         </div>
         <div class="row">
             <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-primary shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Data Pengajar
+                <a href="{{ route('teachers.index') }}" style="text-decoration: none; cursor: pointer;" class="cursor-pointer">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Data Pengajar
+                                    </div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $teachers_count }} Pengajar</div>
                                 </div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $teachers_count }} Pengajar</div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                <div class="col-auto">
+                                    <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
             <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-success shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                    Data Siswa
+                <a href="{{ route('students.index') }}" style="text-decoration: none; cursor: pointer;" class="cursor-pointer">
+                    <div class="card border-left-success shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                        Data Siswa
+                                    </div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $students_count }} Siswa</div>
                                 </div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $students_count }} Siswa</div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                <div class="col-auto">
+                                    <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
             <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-success shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Data Kelas
+                <a href="{{ route('class-group.index') }}" style="text-decoration: none; cursor: pointer;" class="cursor-pointer">
+                    <div class="card border-left-success shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Data Kelas
+                                    </div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $class_count }} Kelas</div>
                                 </div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $class_count }} Kelas</div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                <div class="col-auto">
+                                    <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
             <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-success shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                    Data Karyawan
+                <a href="{{ route('employees.index') }}" style="text-decoration: none; cursor: pointer;" class="cursor-pointer">
+                    <div class="card border-left-success shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                        Data Karyawan
+                                    </div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $employee_count }} Karyawan</div>
                                 </div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $employee_count }} Karyawan</div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                <div class="col-auto">
+                                    <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
         </div>
     </div>
