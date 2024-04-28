344e111116beba7f784c94eae59b397122f54a2c
alvinpradana <alvinpradana14@gmail.com>
2024-04-28 13:38:23 +0700

    update link home dashboard card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,15 +48,15 @@
                 </a>
             </div>
             <div class="col-xl-3 col-md-6 mb-4">
-                <a href="{{ route('class-group.index') }}" style="text-decoration: none; cursor: pointer;" class="cursor-pointer">
+                <a href="{{ route('employees.index') }}" style="text-decoration: none; cursor: pointer;" class="cursor-pointer">
                     <div class="card border-left-success shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                        Data Kelas
+                                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                        Data Karyawan
                                     </div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $class_count }} Kelas</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $employee_count }} Karyawan</div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -67,15 +67,15 @@
                 </a>
             </div>
             <div class="col-xl-3 col-md-6 mb-4">
-                <a href="{{ route('employees.index') }}" style="text-decoration: none; cursor: pointer;" class="cursor-pointer">
+                <a href="{{ route('class-group.index') }}" style="text-decoration: none; cursor: pointer;" class="cursor-pointer">
                     <div class="card border-left-success shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                        Data Karyawan
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Data Kelas
                                     </div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $employee_count }} Karyawan</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $class_count }} Kelas</div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
