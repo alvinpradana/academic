e4850dde9f08018f942edf44db15955e412cbff3
alvinpradana <alvinpradana14@gmail.com>
2024-06-22 22:26:54 +0700

    create profile page

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.app', ['title' => 'View Data Student'])
+
+@section('content')
+    <div>
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Data Profile</h1>
+        </div>
+        <div class="row">
+            <div class="col-md-12 col-sm-12">
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-primary">Detail Data Profile</h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-sm-12 col-md-3">
+                                <div class="card" style="width: 16rem; height: 16rem;">
+                                    <img class="card-img-top" src="..." alt="Profile photo">
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-9">
+                                <div class="row mb-3">
+                                    <div class="col-sm-12 col-md-4 col-lg-4 mb-2">
+                                        <strong>Nama Lengkap</strong>
+                                    </div>
+                                    <div class="col-sm-12 col-md-8 col-lg-8 mb-2 text-capitalize">
+                                        <strong class="mr-2">:</strong> Alvin Novian Perdana
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-sm-12 col-md-4 col-lg-4 mb-2">
+                                        <strong>Nomor Induk Pelajar (NIP)</strong>
+                                    </div>
+                                    <div class="col-sm-12 col-md-8 col-lg-8 mb-2">
+                                        <strong class="mr-2">:</strong> 
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-sm-12 col-md-4 col-lg-4 mb-2">
+                                        <strong>Tempat, Tanggal Lahir</strong>
+                                    </div>
+                                    <div class="col-sm-12 col-md-8 col-lg-8 mb-2 text-capitalize">
+                                        <strong class="mr-2">:</strong> 
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-sm-12 col-md-4 col-lg-4 mb-2">
+                                        <strong>Jenis Kelamin</strong>
+                                    </div>
+                                    <div class="col-sm-12 col-md-8 col-lg-8 mb-2">
+                                        <strong class="mr-2">:</strong>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-sm-12 col-md-4 col-lg-4 mb-2">
+                                        <strong>Usia</strong>
+                                    </div>
+                                    <div class="col-sm-12 col-md-8 col-lg-8 mb-2">
+                                        <strong class="mr-2">:</strong> 
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-sm-12 col-md-4 col-lg-4 mb-2">
+                                        <strong>Agama</strong>
+                                    </div>
+                                    <div class="col-sm-12 col-md-8 col-lg-8 mb-2">
+                                        <strong class="mr-2">:</strong>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-sm-12 col-md-4 col-lg-4 mb-2">
+                                        <strong>Nomor HP</strong>
+                                    </div>
+                                    <div class="col-sm-12 col-md-8 col-lg-8 mb-2">
+                                        <strong class="mr-2">:</strong> 
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-sm-12 col-md-4 col-lg-4 mb-2">
+                                        <strong>Email Aktif</strong>
+                                    </div>
+                                    <div class="col-sm-12 col-md-8 col-lg-8 mb-2">
+                                        <strong class="mr-2">:</strong> 
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-sm-12 col-md-4 col-lg-4 mb-2">
+                                        <strong>Alamat</strong>
+                                    </div>
+                                    <div class="col-sm-12 col-md-8 col-lg-8 mb-2 text-capitalize">
+                                        <strong class="mr-2">:</strong> 
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
