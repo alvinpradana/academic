5e6f3d9b681d1f19cbbddd62fc135c7fac2ed4fd
alvinpradana <alvinpradana14@gmail.com>
2024-09-22 11:21:58 +0700

    rename button export to excel

--- resources/views/scholarships/home.blade.php
+++ resources/views/scholarships/home.blade.php
@@ -6,7 +6,7 @@
             <h1 class="h3 mb-0 text-gray-800">Data Beasiswa</h1>
             @if (Auth::user()->position_id == 3)
                 <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
-                    <i class="fas fa-download fa-sm text-white-50"></i> Generate Report
+                    <i class="fas fa-download fa-sm text-white-50"></i> Export to Excel
                 </a>
             @endif
         </div>
