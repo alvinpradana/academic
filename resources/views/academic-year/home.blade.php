564b8415ce3101444b48551cf0b4fa832b84388f
alvinpradana <alvinpradana14@gmail.com>
2025-02-01 23:25:45 +0700

    disable button 'download excel'

--- resources/views/academic-year/home.blade.php
+++ resources/views/academic-year/home.blade.php
@@ -5,9 +5,9 @@
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Data Tahun Ajaran Sekolah</h1>
             @if (Auth::user()->position_id == 3)
-                <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+                {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
                     <i class="fas fa-download fa-sm text-white-50"></i> Download Excel
-                </a>
+                </a> --}}
             @endif
         </div>
     </div>
--- resources/views/positions/home.blade.php
+++ resources/views/positions/home.blade.php
@@ -4,9 +4,9 @@
     <div>
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Data Jabatan</h1>
-            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+            {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
                 <i class="fas fa-download fa-sm text-white-50"></i> Download Excel
-            </a>
+            </a> --}}
         </div>
     </div>
     @include('positions.table')
